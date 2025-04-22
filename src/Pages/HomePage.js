b5d77e97392e1baa2fee8c321018e31964297559
Rithika <rithikaz2652003@gmail.com>
2025-04-22 17:58:50 +0530

    axios installed

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import { products } from '../data';
-import { Link } from 'react-router-dom';
-import './HomePage.css'; 
-import { useContext } from 'react';
-import { CartContext } from '../CartContext';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+import { useContext } from "react";
+import { CartContext } from "../CartContext";
 
 const HomePage = () => {
   const { addToCart } = useContext(CartContext);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+      });
+  }, []);
+
   return (
     <div style={{ padding: "20px" }}>
       <h2>All Products</h2>
-      <div className="products-container" style={{ 
-        display: "grid", 
-        gap: "20px", 
-        gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))" //makes the grid responsive
-      }}>
-        {products.map(product => (
-          <div key={product.id} className="product-card" style={{ border: "1px solid #ddd", padding: "10px" }}>
-            <img src={product.image} alt={product.name} width="150" className="product-image" />
+      <div
+        className="products-container"
+        style={{
+          display: "grid",
+          gap: "20px",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", //makes the grid responsive
+        }}
+      >
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="product-card"
+            style={{ border: "1px solid #ddd", padding: "10px" }}
+          >
+            <img
+              src={product.image}
+              alt={product.name}
+              width="150"
+              className="product-image"
+            />
             <h4>{product.name}</h4>
-            <p>₹{product.price}</p>            
+            <p>₹{product.price}</p>
             <p>{product.description}</p>
-            <button  className="btn" onClick={() => addToCart(product)}>Add to Cart</button>
+            <button className="btn" onClick={() => addToCart(product)}>
+              Add to Cart
+            </button>
             <div style={{ marginTop: "10px" }}>
               <Link to={`/product/${product.id}`}>View</Link>
             </div>
